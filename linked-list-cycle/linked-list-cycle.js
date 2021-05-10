9fdf9a017d474eb26c542911a6354a4d77a66887
aHumptyDumpty <50850782+aHumptyDumpty@users.noreply.github.com>
2021-05-09 21:11:26 -0500

    Time: 92 ms (32.83%), Space: 40.9 MB (82.69%) - LeetHub

--- linked-list-cycle/linked-list-cycle.js
+++ linked-list-cycle/linked-list-cycle.js
@@ -12,30 +12,19 @@
  */
 var hasCycle = function(head) {
     
-    //edge case if list is empty
-    if(head === null) return false;
-    
     let fast = head;
     let slow = head;
     
-    
-    
-    
-    //iterate through linked list 
     while(fast !== null && fast.next !== null){
-    
         
-        fast = fast.next.next;
+        
         slow = slow.next;
+        fast = fast.next.next;
         
-        //if fast pointer and slow pointer intersect in list return true
-        if(fast === slow) {
-            return true;
-        }
+        if(fast=== slow) return true;
+            
     }
     
-    //return false otherwise
-    return false
-    
+    return false;
     
 };
