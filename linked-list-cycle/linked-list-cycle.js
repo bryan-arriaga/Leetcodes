fa8f98717825287db111ecafac98804a15accebd
aHumptyDumpty <50850782+aHumptyDumpty@users.noreply.github.com>
2021-05-12 16:04:23 -0500

    Time: 88 ms (63.64%), Space: 40.9 MB (81.56%) - LeetHub

--- linked-list-cycle/linked-list-cycle.js
+++ linked-list-cycle/linked-list-cycle.js
@@ -12,17 +12,15 @@
  */
 var hasCycle = function(head) {
     
+    
     let fast = head;
     let slow = head;
     
     while(fast !== null && fast.next !== null){
-        
-        
-        slow = slow.next;
         fast = fast.next.next;
+        slow = slow.next;
         
-        if(fast=== slow) return true;
-            
+        if(fast === slow) return true
     }
     
     return false;
