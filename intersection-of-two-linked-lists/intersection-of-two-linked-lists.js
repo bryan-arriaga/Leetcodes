ae3467b421a0142cd5641ce21bd87649f23a15e5
aHumptyDumpty <50850782+aHumptyDumpty@users.noreply.github.com>
2021-05-12 21:26:55 -0500

    Time: 100 ms (93.80%), Space: 46 MB (73.65%) - LeetHub

--- intersection-of-two-linked-lists/intersection-of-two-linked-lists.js
+++ intersection-of-two-linked-lists/intersection-of-two-linked-lists.js
@@ -13,32 +13,27 @@
  */
 var getIntersectionNode = function(headA, headB) {
     
-    let a_node = headA;
-    let b_node = headB;
+    let firstList = headA;
+    let secondList = headB;
     
-    //while nodes are not the same traverse
-    while(a_node !== b_node){
-        
-        //if a node points to null traverse b list else traverse otherwise
-        if(a_node === null){
-            a_node = headB;
-        } else {    
-            a_node = a_node.next;    
-        }
-        
-        //if b node points to null traverse a list else traverse otherwise
-        if(b_node === null){
-            b_node = headA;
+    while(firstList !== secondList){
+     
+        if(firstList === null){
+            firstList = headB;
         } else {
-            b_node = b_node.next;
+            firstList = firstList.next;
         }
-    
-    
+        
+        
+        if(secondList === null){
+            secondList = headA;
+        } else {
+            secondList = secondList.next;
+        }
+        
+        
     }
     
-    
-    
-    //return intersection at end
-    return a_node;
+    return firstList;
     
 };
