3fd841bb64b7e6dfeed38e4c6b5040cc2d6de4a2
aHumptyDumpty <50850782+aHumptyDumpty@users.noreply.github.com>
2021-05-14 13:06:07 -0500

    Time: 84 ms (91.85%), Space: 40.2 MB (69.85%) - LeetHub

--- merge-two-sorted-lists/merge-two-sorted-lists.js
+++ merge-two-sorted-lists/merge-two-sorted-lists.js
@@ -12,12 +12,10 @@
  */
 var mergeTwoLists = function(l1, l2) {
     
-    //empty list to hold merged nodes from l1 + l2
     let merge = new ListNode(-1);
     let head = merge;
+
     
-    
-    //iterate through both l1 and l2 as long as there are values in either list
     while(l1 !== null && l2 !== null){
         
         if(l1.val < l2.val){
@@ -25,21 +23,18 @@ var mergeTwoLists = function(l1, l2) {
             l1 = l1.next;
         } else {
             merge.next = l2;
-            l2 = l2.next;
+            l2 = l2.next
         }
         
-        merge = merge.next;
+        merge = merge.next
     }
     
     if(l1 !== null){
-        merge.next = l1
+        merge.next = l1;
     } else {
         merge.next = l2;
     }
     
-    //return merged list
+    
     return head.next;
-    
-    
-    
 };
