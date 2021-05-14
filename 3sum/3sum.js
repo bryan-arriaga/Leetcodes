be025cbb802b84fe309a3c7c529bb1b77c89645e
aHumptyDumpty <50850782+aHumptyDumpty@users.noreply.github.com>
2021-05-13 20:50:01 -0500

    Time: 152 ms (58.68%), Space: 48.5 MB (88.19%) - LeetHub

--- 3sum/3sum.js
+++ 3sum/3sum.js
@@ -4,25 +4,20 @@
  */
 var threeSum = function(nums) {
     
-
-    //sort array before traversal
-    nums.sort((a , b) => a - b);
+    nums.sort((a, b) => a - b);
     
     let triplets = [];
     
-    //iterate through nums
     for(let i = 0; i < nums.length; i++){
+        if(i > 0 && nums[i] === nums[i -1]) continue;
         
-        if(i > 0 && nums[i] === nums[i - 1]) continue;
-        
-        const target = 0 - nums[i]
+        let target = 0 - nums[i];
         
         let left = i + 1;
         let right = nums.length - 1;
         
         while(right > left){
-            
-            const sum = nums[left] + nums[right];
+            let sum = nums[left] + nums[right];
             
             if(sum > target){
                 right--;
@@ -31,15 +26,17 @@ var threeSum = function(nums) {
             } else {
                 triplets.push([nums[i], nums[left], nums[right]]);
                 
-                //skip duplicates while iterating
+                //while there are duplicates skip over them by incrementing left pointer/right pointer
                 while(nums[left] === nums[left + 1]) left++;
-                while(nums[right] === nums[right -1]) right--;
+                while(nums[right] === nums[right - 1]) right--;
+                
+                //increment/decrement pointers otherwise 
                 left++;
                 right--;
             }
         }
-
     }
+    
     
     return triplets;
     
