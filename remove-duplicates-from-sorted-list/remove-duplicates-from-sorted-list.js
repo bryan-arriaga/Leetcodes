96f0477e8ceef851634c38849c4213878288426d
aHumptyDumpty <50850782+aHumptyDumpty@users.noreply.github.com>
2021-05-14 12:59:31 -0500

    Time: 84 ms (92.83%), Space: 40.1 MB (94.96%) - LeetHub

--- remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.js
+++ remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.js
@@ -15,10 +15,11 @@ var deleteDuplicates = function(head) {
     
     while(node !== null && node.next !== null){
         if(node.val === node.next.val){
-            node.next = node.next.next;
-        } else {
-            node = node.next;
+            node.next = node.next.next
+        } else{
+            node = node.next    
         }
+        
     }
     
     return head;
