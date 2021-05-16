1c5fb3a54a9e8fc02a3149c8dd3eed8d61c20e94
aHumptyDumpty <50850782+aHumptyDumpty@users.noreply.github.com>
2021-05-16 16:50:02 -0500

    Time: 92 ms (68.64%), Space: 48.2 MB (18.37%) - LeetHub

--- container-with-most-water/container-with-most-water.js
+++ container-with-most-water/container-with-most-water.js
@@ -7,10 +7,13 @@ var maxArea = function(height) {
     let max = 0;
     let i = 0;
     let j = height.length - 1;
-
+    
+    
     while(i < j){
         let min = Math.min(height[i], height[j]);
+        
         max = Math.max(max, min * (j - i));
+        
         if(height[i] < height[j]){
             i++;
         } else {
@@ -19,4 +22,5 @@ var maxArea = function(height) {
     }
     
     return max;
+    
 };
