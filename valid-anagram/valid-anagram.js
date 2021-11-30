2ff0bbfe77241b5601483f0248e8654e4e7f1967
Bryan Arriaga <50850782+aHumptyDumpty@users.noreply.github.com>
2021-11-29 18:44:44 -0600

    Time: 92 ms (72.61%), Space: 40 MB (91.50%) - LeetHub

--- valid-anagram/valid-anagram.js
+++ valid-anagram/valid-anagram.js
@@ -5,34 +5,40 @@
  */
 var isAnagram = function(s, t) {
     
+    
+    //test to see if both s and t are the same length return false
     if(s.length !== t.length) return false;
     
-    //create map for constant lookup time
-    let map = new Map();
+    //make two objs to store inputs from both s and t 
+    let map = {}
     
-    //iterate through s and t passing letters to map
-    
+    //loop through and add characters from s input
     for(let i = 0; i < s.length; i++){
         let letter = s[i];
+        
         if(!map[letter]){
             map[letter] = 1;
         } else {
             map[letter]++;
         }
+    
     }
     
-    //iterate through t and compare letters to whats already inside map
+    //loop through and add characters from t input
     for(let i = 0; i < t.length; i++){
-        let letter = t[i];
-        if(!map[letter]){
-            return false;
-            map[letter] = 1;
-        } else {
+        let letter = t[i]
+    //if we encounter a letter in s input thats already in map
+        if(map[letter]){
+            //decrease the value
             map[letter]--;
+        } else {
+        //else return false
+            return false;
         }
-    }
+        
+        
+     }
     
-    //return true at end
-    return true
+    return true;
     
 };
