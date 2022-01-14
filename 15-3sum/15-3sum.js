bc636f28ef39667f627bedb310d207089195c5ae
Bryan Arriaga <50850782+aHumptyDumpty@users.noreply.github.com>
2022-01-13 21:26:33 -0600

    Time: 140 ms (85.57%), Space: 49.2 MB (52.84%) - LeetHub

---
+++ 15-3sum/15-3sum.js
@@ -0,0 +1,42 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var threeSum = function(nums) {
+    
+    //sort given nums array
+    nums.sort((a,b ) => a - b);
+    
+    //initialize triplets array to hold answer 
+    let triplets = [];
+    
+    //iterate through given nums array
+    for(let i = 0; i < nums.length; i++){
+        if(i > 0 && nums[i] === nums[i - 1]) continue;
+        
+        let target = 0 - nums[i];
+        
+        let left = i + 1;
+        let right = nums.length - 1;
+        
+        while(left < right){
+            let sum = nums[left] + nums[right];
+            if(sum < target){
+                left++;
+            } else if(sum > target){
+                right --;
+            } else {
+                triplets.push([nums[i], nums[left], nums[right]]);
+                
+                while(nums[left] === nums[left + 1]) left++;
+                while(nums[right] === nums[right - 1]) right--;
+                left++;
+                right--;
+            }
+        }
+    }
+    
+    //return triplets array at end
+    return triplets;
+    
+};
