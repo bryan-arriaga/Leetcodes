53ee8e3d4df606e02112be0b297a2a25e6fbc13f
Bryan Arriaga <50850782+aHumptyDumpty@users.noreply.github.com>
2022-03-08 19:31:15 -0600

    Time: 196 ms (30.43%), Space: 61.6 MB (37.55%) - LeetHub

--- 187-repeated-dna-sequences/187-repeated-dna-sequences.js
+++ 187-repeated-dna-sequences/187-repeated-dna-sequences.js
@@ -5,22 +5,23 @@
 var findRepeatedDnaSequences = function(s) {
     
     let subStrings = {};
-    let output = [];
+    let result = [];
     let i = 0;
     
     while(i < s.length){
-        const dnaSequence = s.slice(i, i + 10);
+        let dnaSequence = s.slice(i, i + 10);
         
         if(!subStrings[dnaSequence]){
             subStrings[dnaSequence] = 1;
         } else {
             subStrings[dnaSequence]++;
         }
-        i++
         
-        if(subStrings[dnaSequence] === 2) output.push(dnaSequence);
+        if(subStrings[dnaSequence]=== 2) result.push(dnaSequence);
+        
+        i++;
     }
     
-    return output;
+    return result;
     
 };
