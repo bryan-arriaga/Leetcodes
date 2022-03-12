b2ff47adaea0bd947bc81c45136f66d95d174565
Bryan Arriaga <50850782+aHumptyDumpty@users.noreply.github.com>
2022-03-11 19:56:29 -0600

    Time: 62 ms (94.11%), Space: 42.9 MB (29.80%) - LeetHub

--- 844-backspace-string-compare/844-backspace-string-compare.js
+++ 844-backspace-string-compare/844-backspace-string-compare.js
@@ -4,13 +4,14 @@
  * @return {boolean}
  */
 var backspaceCompare = function(s, t) {
-        return edit(s) === edit(t);
+    
+    return edit(s) === edit(t);
     
     function edit(str) {
         let result = '';
         let backspaces = 0;
         
-        for (let i = str.length - 1; i >= 0; i -= 1) {        
+        for (let i = str.length - 1; i >= 0; i--) {        
             if (str[i] === '#') {
                 backspaces += 1;
             } else if (backspaces > 0) {
