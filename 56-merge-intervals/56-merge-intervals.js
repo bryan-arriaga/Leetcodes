bade4615728a88ad84435c0c1315bd90a6d5eb4d
Bryan Arriaga <50850782+aHumptyDumpty@users.noreply.github.com>
2022-03-17 17:13:28 -0500

    Time: 161 ms (49.04%), Space: 49.5 MB (40.41%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.js
+++ 56-merge-intervals/56-merge-intervals.js
@@ -5,8 +5,8 @@
 var merge = function(intervals) {
     
     intervals.sort((a,b) => a[0] - b[0]);
-    let stack = [];
     
+    let stack = [];
     stack.push(intervals[0]);
     
     for(let i = 1; i < intervals.length; i++){
@@ -14,7 +14,7 @@ var merge = function(intervals) {
         let curr = intervals[i];
         
         if(prev[1] >= curr[0]){
-            stack.push([prev[0], Math.max(prev[1], curr[1])])
+            stack.push([prev[0], Math.max(prev[1], curr[1])]);
         } else {
             stack.push(prev, curr);
         }
