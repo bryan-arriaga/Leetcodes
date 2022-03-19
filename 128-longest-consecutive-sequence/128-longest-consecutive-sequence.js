ece513b41073eb3dc24d941d5f686c56495a52ba
Bryan Arriaga <50850782+aHumptyDumpty@users.noreply.github.com>
2022-03-19 18:15:09 -0500

    Time: 148 ms (61.24%), Space: 50.6 MB (51.06%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.js
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.js
@@ -1,22 +1,50 @@
-
-function longestConsecutive(nums) {
-  if (nums == null || nums.length === 0) return 0;
-  
-  const set = new Set(nums);
-  let max = 0;
-
-  for (let num of set) {
-    if (set.has(num - 1)) continue;  // make sure starting from the beginning of sequence
-
-    let currNum = num;
-    let currMax = 1;
-
-    while (set.has(currNum + 1)) {
-      currNum++;
-      currMax++;
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var longestConsecutive = function(nums) {
+    
+    if(nums.length === 0 || nums === null) return 0;
+    
+    const set = new Set(nums);
+    let max = 0;
+    
+    for(let num of set){
+        if(set.has(num - 1)) continue;
+        
+        let currNum = num;
+        let currMax = 1;
+        
+        while(set.has(currNum + 1)){
+            currNum++;
+            currMax++;
+        }
+        max = Math.max(max, currMax);
+        
     }
-    max = Math.max(max, currMax);
-  }
+    
+    
+    return max;
+};
 
-  return max;
-}
+// function longestConsecutive(nums) {
+//   if (nums == null || nums.length === 0) return 0;
+  
+//   const set = new Set(nums);
+//   let max = 0;
+
+//   for (let num of set) {
+//     if (set.has(num - 1)) continue;  // make sure starting from the beginning of sequence
+
+//     let currNum = num;
+//     let currMax = 1;
+
+//     while (set.has(currNum + 1)) {
+//       currNum++;
+//       currMax++;
+//     }
+//     max = Math.max(max, currMax);
+//   }
+
+//   return max;
+// }
