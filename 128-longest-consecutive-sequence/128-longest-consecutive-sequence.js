aa1066bf4082dc35eea3192b95b6774ce7708eb4
Bryan Arriaga <50850782+aHumptyDumpty@users.noreply.github.com>
2022-03-19 18:25:25 -0500

    Time: 98 ms (89.30%), Space: 50.8 MB (43.88%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.js
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.js
@@ -6,45 +6,24 @@ var longestConsecutive = function(nums) {
     
     if(nums.length === 0 || nums === null) return 0;
     
-    const set = new Set(nums);
+    let set = new Set(nums);
     let max = 0;
     
     for(let num of set){
         if(set.has(num - 1)) continue;
         
-        let currNum = num;
         let currMax = 1;
+        let currNum = num;
         
         while(set.has(currNum + 1)){
-            currNum++;
             currMax++;
+            currNum++;
         }
         max = Math.max(max, currMax);
-        
     }
     
-    
     return max;
+    
 };
 
-// function longestConsecutive(nums) {
-//   if (nums == null || nums.length === 0) return 0;
-  
-//   const set = new Set(nums);
-//   let max = 0;
-
-//   for (let num of set) {
-//     if (set.has(num - 1)) continue;  // make sure starting from the beginning of sequence
-
-//     let currNum = num;
-//     let currMax = 1;
-
-//     while (set.has(currNum + 1)) {
-//       currNum++;
-//       currMax++;
-//     }
-//     max = Math.max(max, currMax);
-//   }
-
-//   return max;
-// }
+///100 200  1 -2 - 3- 4
