62256356316ee9346484578a84372a1aa744573b
Bryan Arriaga <50850782+aHumptyDumpty@users.noreply.github.com>
2022-03-20 18:19:34 -0500

    Time: 96 ms (31.96%), Space: 42.1 MB (57.15%) - LeetHub

--- 48-rotate-image/48-rotate-image.js
+++ 48-rotate-image/48-rotate-image.js
@@ -3,17 +3,16 @@
  * @return {void} Do not return anything, modify matrix in-place instead.
  */
 var rotate = function(matrix) {
-    
+
     for(let r = 0; r < matrix.length; r++){
         for(let c = r; c < matrix[0].length; c++){
             [matrix[r][c], matrix[c][r]] = [matrix[c][r], matrix[r][c]];
-        }
+        }        
     }
     
-    for(let num of matrix){
-        num.reverse();
+    for(let row of matrix){
+        row.reverse();
     }
     
-    return matrix
-    
+    return matrix;
 };
