2ab056283a42dfeb082c1b5b8fd915f82eb87256
Bryan Arriaga <50850782+aHumptyDumpty@users.noreply.github.com>
2022-03-23 15:04:23 -0500

    Time: 72 ms (92.11%), Space: 44.2 MB (70.93%) - LeetHub

--- 57-insert-interval/57-insert-interval.js
+++ 57-insert-interval/57-insert-interval.js
@@ -3,38 +3,26 @@
  * @param {number[]} newInterval
  * @return {number[][]}
  */
-var insert = function (intervals, newInterval) {
-    if (intervals.length === 0) {
-        return [ newInterval ];
-    }
-
-    const result = [];
-    let flag = 0;
-    let i;
-
-    for (i = 0; i < intervals.length; i++) {
-        const interval = intervals[i];
-        if (newInterval[0] > interval[1]){
-            result.push(interval);
-        } else if (newInterval[1] < interval[0]){
+var insert = function(intervals, newInterval) {
+    
+    let result = [];
+    
+    for(let [start,end] of intervals){
+        if(!newInterval || end < newInterval[0]){
+            result.push([start,end]);
+        } else if(newInterval[1] < start){
             result.push(newInterval);
-            flag = 1;
-            break;
+            newInterval = null;
+            result.push([start,end])
         } else {
-            newInterval[0] = Math.min(newInterval[0], interval[0]);
-            newInterval[1] = Math.max(newInterval[1], interval[1]);
+            newInterval[0] = Math.min(newInterval[0], start);
+            newInterval[1] = Math.max(newInterval[1], end);
         }
     }
-
-    if (flag === 0) {
+    
+    if(newInterval){
         result.push(newInterval);
-        return result;
-    } else {
-        return result.concat(intervals.splice(i));
     }
     
-    return arr;
+    return result
 };
-    
-    
-    
