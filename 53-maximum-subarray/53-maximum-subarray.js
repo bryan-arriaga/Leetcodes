695a1bbf900ae3b73792f72e9236e12592e66b74
Bryan Arriaga <50850782+aHumptyDumpty@users.noreply.github.com>
2022-03-27 17:30:00 -0500

    Time: 76 ms (95.66%), Space: 50.8 MB (21.35%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.js
+++ 53-maximum-subarray/53-maximum-subarray.js
@@ -3,20 +3,18 @@
  * @return {number}
  */
 var maxSubArray = function(nums) {
-
-    if(nums.length === 0 || nums === null) return 0;
     
     let sum = 0;
     let maxSub = nums[0];
     
     for(let num of nums){
-        if(sum < 0){
-            sum = 0;
+        if(sum <= 0){
+            sum  = 0;
         }
         sum += num
-        maxSub = Math.max(maxSub, sum)
+        maxSub = Math.max(maxSub, sum);
     }
     
-    return maxSub;
+    return maxSub
     
 };
