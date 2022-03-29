7af44347681886d2fe36553caf744f851e7c34e1
Bryan Arriaga <50850782+aHumptyDumpty@users.noreply.github.com>
2022-03-29 14:52:06 -0500

    Time: 92 ms (70.43%), Space: 44.4 MB (78.07%) - LeetHub

--- 253-meeting-rooms-ii/253-meeting-rooms-ii.js
+++ 253-meeting-rooms-ii/253-meeting-rooms-ii.js
@@ -4,37 +4,34 @@
  */
 var minMeetingRooms = function(intervals) {
     
-   
-    
-    if(intervals.length === 0) return 0;
+    if(intervals.length === 0 || intervals === null) return 0;
     
     let start = [intervals.length];
     let end = [intervals.length];
-    
+
     for(let i = 0; i < intervals.length; i++){
         start[i] = intervals[i][0];
         end[i] = intervals[i][1];
+        
     }
- 
     
     start.sort((a, b) => a - b);
-    end.sort((a, b) => a -b)
+    end.sort((a, b) => a - b);
     
+    let meetingRooms = 0;
     let startP = 0;
     let endP = 0;
-     
-    let usedRooms = 0;
+ 
     while(startP < intervals.length){
+        
         if(start[startP] >= end[endP]){
-           usedRooms--;
-            endP++
+            endP++;
+            meetingRooms--;
         }
-        usedRooms++
         startP++;
+        meetingRooms++;
     }
     
-
-    
-    return usedRooms;
+    return meetingRooms;
     
 };
