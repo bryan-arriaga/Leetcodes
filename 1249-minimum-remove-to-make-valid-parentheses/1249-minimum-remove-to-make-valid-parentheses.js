693c1c1ad976265eec77c4923a3b99929aa979c5
Bryan Arriaga <50850782+aHumptyDumpty@users.noreply.github.com>
2022-04-06 16:47:12 -0500

    Time: 137 ms (43.74%), Space: 48.5 MB (91.31%) - LeetHub

---
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var minRemoveToMakeValid = function(str) {
+    
+    str = str.split("");
+    let stack = [];
+    
+    for(let i = 0; i < str.length; i++){
+        if(str[i] === "(") stack.push(i);
+        
+        else if(str[i] === ")"){
+            if(stack.length) stack.pop();
+            else str[i] = ""
+        }
+    }
+    
+    
+    for(let i of stack) str[i] = ""
+    
+    return str.join("");
+    
+};
+
