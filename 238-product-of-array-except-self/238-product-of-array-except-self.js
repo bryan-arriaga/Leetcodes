6e9302690669cbdd83e756cfdb7a56d393040da8
Bryan Arriaga <50850782+aHumptyDumpty@users.noreply.github.com>
2022-04-11 16:33:40 -0500

    Time: 149 ms (47.32%), Space: 55.6 MB (30.61%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.js
+++ 238-product-of-array-except-self/238-product-of-array-except-self.js
@@ -4,27 +4,22 @@
  */
 var productExceptSelf = function(nums) {
     
-    let left = [];
-    let right = [nums.length];
     let answer = [];
+    let multiplier = 1;
     
-    left[0] = 1;
+    for(let i = 0; i < nums.length; i++){
+        answer[i] = multiplier
+        multiplier *= nums[i];
+    } 
+    multiplier = 1;
     
-    for(let i = 1; i < nums.length; i++){
-        left[i] = left[i - 1] * nums[i - 1]
-    }
-    
-    
-    right[nums.length - 1] = 1;
-    
-    for(let i = nums.length - 2; i >= 0; i--){
-        right[i] = right[i + 1] * nums[i + 1];
-    }
-    
-    for(let i =0; i < nums.length; i++){
-        answer[i] = left[i] * right[i]
+    for(let i = nums.length -1; i >= 0; i--){
+        answer[i] *= multiplier;
+        multiplier *= nums[i];
     }
     
     return answer
+    
+    
     
 };
