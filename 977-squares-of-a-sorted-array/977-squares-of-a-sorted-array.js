e3facff18348e1f073bbb044f43ff16d5a2efb19
Bryan Arriaga <50850782+aHumptyDumpty@users.noreply.github.com>
2022-04-18 18:36:16 -0500

    Time: 105 ms (84.68%), Space: 48.6 MB (67.42%) - LeetHub

--- 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.js
+++ 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.js
@@ -3,21 +3,21 @@
  * @return {number[]}
  */
 var sortedSquares = function(nums) {
-    
-    let newArr = [nums.length];
+
+    let answer = [nums.length];
     let left = 0;
     let right = nums.length - 1;
     
     for(let i = nums.length - 1; i >= 0; i--){
         if(Math.abs(nums[left]) < Math.abs(nums[right])){
-            newArr[i] = nums[right] ** 2;
+            answer[i] = nums[right] ** 2;
             right--;
         } else {
-            newArr[i] = nums[left] ** 2;
+            answer[i] = nums[left] ** 2;
             left++;
         }
     }
     
-    return newArr;
+    return answer;
     
 };
