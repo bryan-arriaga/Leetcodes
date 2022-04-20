42daa3a855a2d885c6b211656be168565f8ea6ee
Bryan Arriaga <50850782+aHumptyDumpty@users.noreply.github.com>
2022-04-20 16:10:26 -0500

    Time: 339 ms (27.23%), Space: 98.6 MB (48.42%) - LeetHub

--- 111-minimum-depth-of-binary-tree/111-minimum-depth-of-binary-tree.js
+++ 111-minimum-depth-of-binary-tree/111-minimum-depth-of-binary-tree.js
@@ -10,22 +10,43 @@
  * @param {TreeNode} root
  * @return {number}
  */
-var minDepth = function(root) {
-      if (!root) return 0
+// var minDepth = function(root) {
+//       if (!root) return 0
     
-    const q = [root]
-    let level = 0
+//     const q = [root]
+//     let level = 0
 
-    while (q.length > 0) {
-        level++
-        const len = q.length
-        for (let i = 0; i < len; i++) {
-            let node = q.pop()
-            if (node.left === null && node.right === null) return level
-            if (node.left) q.unshift(node.left)
-            if (node.right) q.unshift(node.right)
+//     while (q.length > 0) {
+//         level++
+//         const len = q.length
+//         for (let i = 0; i < len; i++) {
+//             let node = q.pop()
+//             if (node.left === null && node.right === null) return level
+//             if (node.left) q.unshift(node.left)
+//             if (node.right) q.unshift(node.right)
+//         }
+//     }
+    
+//     return level
+// };
+ 
+var minDepth = function(root) {
+     if(root === null) return 0
+    
+    let q = [root];
+    let level = 0;
+    
+    while(q.length > 0 ){
+        level++;
+        let len = q.length;
+        for(let i = 0; i < len; i++){
+            let node = q.pop();
+            if(node.left === null && node.right === null) return level;
+            if(node.left) q.unshift(node.left);
+            if(node.right) q.unshift(node.right);
         }
     }
     
     return level
+    
 };
