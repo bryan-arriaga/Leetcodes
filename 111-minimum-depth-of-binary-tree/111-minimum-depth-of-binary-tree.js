4d194efd317cbec74b805f9d7555bfb482ea9499
Bryan Arriaga <50850782+aHumptyDumpty@users.noreply.github.com>
2022-04-20 15:44:48 -0500

    Time: 264 ms (68.02%), Space: 97.9 MB (64.82%) - LeetHub

--- 111-minimum-depth-of-binary-tree/111-minimum-depth-of-binary-tree.js
+++ 111-minimum-depth-of-binary-tree/111-minimum-depth-of-binary-tree.js
@@ -20,7 +20,7 @@ var minDepth = function(root) {
         level++
         const len = q.length
         for (let i = 0; i < len; i++) {
-            const node = q.pop()
+            let node = q.pop()
             if (node.left === null && node.right === null) return level
             if (node.left) q.unshift(node.left)
             if (node.right) q.unshift(node.right)
