b5d187cc29b35293e88203e78174600ee5d382d2
Bryan Arriaga <50850782+aHumptyDumpty@users.noreply.github.com>
2022-05-04 16:59:18 -0500

    Time: 63 ms (93.03%), Space: 45.3 MB (36.33%) - LeetHub

---
+++ 704-binary-search/704-binary-search.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+    
+    if(nums.length <= 0 || nums === null);
+    
+    let left = 0;
+    let right = nums.length - 1;
+    
+    while(left <= right){
+        
+        let middle = left + Math.floor((right - left) / 2);
+        
+        if(nums[middle] === target){
+            return middle;
+        } else if(nums[middle] < target){
+            left = middle + 1
+        } else {
+            right = middle - 1  
+        }
+    }
+    return -1;
+    
+};
