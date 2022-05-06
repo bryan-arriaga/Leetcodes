de96c198fc96549e815586283b2d1c109a51c199
Bryan Arriaga <50850782+aHumptyDumpty@users.noreply.github.com>
2022-05-06 15:19:28 -0500

    Time: 176 ms (30.79%), Space: 42.2 MB (92.83%) - LeetHub

--- 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.js
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.js
@@ -4,29 +4,33 @@
  * @return {number}
  */
 var characterReplacement = function(s, k) {
-    if(s.length <= 0 || s === null);
     
+    if(s.length <= 0 || s === null) return -1
+
     let left = 0;
     let right = 0;
-    let answer = 0;
     let maxFreq = 0;
+    let answer = 0;
     
+        
     let hash = {};
     
-    for(let i = 0; i < s.length; i++){
-        hash[s[i]] = hash[s[i]] + 1 || 1;
-        maxFreq = Math.max(maxFreq, hash[s[i]]);
+    for(let i = 0; i < s.length ; i++){
+        hash[s[right]] = hash[s[right]] + 1 || 1;
+        maxFreq = Math.max(maxFreq, hash[s[right]])
+        
         
         while(right - left + 1 - maxFreq > k){
             hash[s[left]]--;
             left++;
         }
-        
         answer = Math.max(answer, right - left + 1);
         right++;
+        
     }
     
     return answer;
+    
     
     
 };
