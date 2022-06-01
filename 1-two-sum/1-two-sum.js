0796d8850638e71ca515bf904823e185cee415e7
Bryan Arriaga <50850782+aHumptyDumpty@users.noreply.github.com>
2022-06-01 16:44:06 -0500

    Time: 78 ms (81.96%), Space: 43.4 MB (24.33%) - LeetHub

--- 1-two-sum/1-two-sum.js
+++ 1-two-sum/1-two-sum.js
@@ -8,10 +8,10 @@ var twoSum = function(nums, target) {
     let map = new Map();
     
     for(let i = 0; i < nums.length; i++){
-        let complement = target - nums[i];
+        let difference = target - nums[i];
         
-        if(map.has(complement)){
-            return [map.get(complement), i];
+        if(map.has(difference)){
+            return [map.get(difference), i];
         } else {
             map.set(nums[i], i);
         }
