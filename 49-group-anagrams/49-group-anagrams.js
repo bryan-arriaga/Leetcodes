908b5d0ae66153facc89ba55c506d5077ec5f867
Bryan Arriaga <50850782+aHumptyDumpty@users.noreply.github.com>
2022-06-02 18:17:16 -0500

    Time: 240 ms (21.06%), Space: 52.5 MB (84.96%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.js
+++ 49-group-anagrams/49-group-anagrams.js
@@ -4,19 +4,24 @@
  */
 var groupAnagrams = function(strs) {
     
-    let map = {};
+    let result = [];
+    let map = new Map();
     
     for(let i = 0; i < strs.length; i++){
-        let str = strs[i];
-        let sorted = str.split('').sort().join('');
+        const sorted =  strs[i].split("").sort().join("");
         
-        if(!map[sorted]){
-            map[sorted] = [str];
+        if(map.has(sorted)){
+            map.get(sorted).push(strs[i]);
         } else {
-            map[sorted].push(str);
+            map.set(sorted, [strs[i]]);
         }
     }
     
-    return Object.values(map);
+    
+    for(let [key, value] of map){
+        result.push(value)
+    } 
+    
+    return result;
     
 };
