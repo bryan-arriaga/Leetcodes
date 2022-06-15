9d4563bb9c35736bd6ddafaa5cfe670d91188272
Bryan Arriaga <50850782+aHumptyDumpty@users.noreply.github.com>
2022-06-15 12:44:49 -0500

    Time: 139 ms (14.76%), Space: 49.7 MB (53.43%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.js
+++ 11-container-with-most-water/11-container-with-most-water.js
@@ -4,21 +4,22 @@
  */
 var maxArea = function(height) {
     
-    let max = 0;
     let i = 0;
-    let j = height.length - 1 
+    let j = height.length - 1;
+    let max = 0;
     let curr;
     
     while(i < j){
-        curr = (j - i) * Math.min(height[i], height[j]);
-        max = Math.max(max, curr);
-        if(height[i]  > height[j]){
-            j--
-        } else {
-            i++
-        }
-    }
+      curr = (j - i) * Math.min(height[i], height[j]);
+      max = Math.max(max, curr);
+      if(height[i] > height[j]){
+          j--;
+      } else {
+          i++
+      }
+  }
     
     return max;
+    
     
 };
