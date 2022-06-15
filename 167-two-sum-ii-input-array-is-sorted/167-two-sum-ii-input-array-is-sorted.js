1c41cfcd666c6db9af10ef5c87c07d527162d3e4
Bryan Arriaga <50850782+aHumptyDumpty@users.noreply.github.com>
2022-06-15 12:41:24 -0500

    Time: 86 ms (56.03%), Space: 43.2 MB (36.44%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.js
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.js
@@ -8,13 +8,15 @@ var twoSum = function(nums, target) {
     let left = 0;
     let right = nums.length - 1;
     
-    while(left < right){
-        let currSum = nums[left] + nums[right];
-        if(currSum > target) right--
-        else if(currSum < target) left++;
-        else return [left + 1, right + 1];
-    };
+    while(nums[left] + nums[right] !== target){
+        if(nums[left] + nums[right] > target){
+            right--;
+        } else {
+            left++
+        }
+    }
     
-    return []
+    
+    return [left + 1, right + 1]
     
 };
