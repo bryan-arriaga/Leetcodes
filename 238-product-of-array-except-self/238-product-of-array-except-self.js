386078640a55b9dac0023d23218ff7e708cd1d76
Bryan Arriaga <50850782+aHumptyDumpty@users.noreply.github.com>
2022-07-19 18:55:06 -0500

    Time: 135 ms (70.41%), Space: 55.5 MB (34.09%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.js
+++ 238-product-of-array-except-self/238-product-of-array-except-self.js
@@ -3,21 +3,22 @@
  * @return {number[]}
  */
 var productExceptSelf = function(nums) {
-    
+
     let result = [];
     let multiplier = 1;
     
     for(let i = 0; i < nums.length; i++){
-        result[i] = multiplier;
-        multiplier *= nums[i];
+        result[i] = multiplier
+        multiplier *= nums[i]
     }
     
     multiplier = 1;
     
-    for(let i = nums.length -1; i >= 0; i--){
-        result[i] *= multiplier;
-        multiplier *= nums[i];
+    for(let i = nums.length - 1; i >= 0; i--){
+        result[i] *= multiplier; 
+        multiplier *= nums[i]
     }
-
+    
     return result;
+    
 };
