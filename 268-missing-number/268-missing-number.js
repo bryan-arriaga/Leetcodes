58fcea02d1871f020a7b76091cd2e99470227502
Bryan Arriaga <50850782+aHumptyDumpty@users.noreply.github.com>
2022-07-19 18:14:04 -0500

    Time: 59 ms (99.41%), Space: 44.3 MB (58.44%) - LeetHub

---
+++ 268-missing-number/268-missing-number.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var missingNumber = function(nums) {
+    
+    let sum = 0;
+    
+    for(let i = 0; i < nums.length; i++){
+        sum += i + 1 - nums[i];
+    }
+
+    return sum;
+    
+};
