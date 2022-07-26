bd9507ca715074b0e865b1a0239eea0dd6643faf
Bryan Arriaga <50850782+aHumptyDumpty@users.noreply.github.com>
2022-07-26 15:52:10 -0500

    Time: 89 ms (90.11%), Space: 44.9 MB (70.92%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.js
+++ 200-number-of-islands/200-number-of-islands.js
@@ -2,32 +2,30 @@
  * @param {character[][]} grid
  * @return {number}
  */
-const numIslands = function (grid) {
-	let numOfIslands = 0
-	//iterate through each island from the top left corner
-	for (let row = 0; row < grid.length; row++) {
-		for (let col = 0; col < grid[0].length; col++) {
-			//if the tile is equal to 1 
-			if (grid[row][col] === "1") {
-				dfs(row, col)
-				//add one to the island count
-				numOfIslands++
-			}
-		}
-	}
-	function dfs(row, col) {
-		//if the tile is in bounds and land
-		if (grid?.[row]?.[col] === "1") {
-			//set the tile to an arbitrary value that isn't "1" or undefined
-			grid[row][col] = "2"
-
-			//check each horizonantal and vertical neighbor
-			dfs(row + 1, col)
-			dfs(row - 1, col)
-			dfs(row, col + 1)
-			dfs(row, col - 1)
-		}
-	}
-	//return numOfIslands
-	return numOfIslands
-}
+var numIslands = function(grid) {
+    
+    let numOfIslands = 0;
+    
+    for(let row = 0; row < grid.length; row++){
+        for(let col = 0; col < grid[0].length; col++){
+            if(grid[row][col] === "1"){
+                dfs(row, col);
+                numOfIslands++;
+            }
+        }
+    }
+    
+    function dfs(row, col){
+        if(grid?.[row]?.[col] === "1"){
+            grid[row][col] = "2";
+            
+            dfs(row + 1, col);
+            dfs(row - 1, col);
+            dfs(row, col + 1);
+            dfs(row, col - 1);
+        }
+    }
+    
+    return numOfIslands;
+    
+};
