54063649dd615be3058593f382e6befcd951986e
Bryan Arriaga <50850782+aHumptyDumpty@users.noreply.github.com>
2022-08-01 19:45:37 -0500

    Time: 128 ms (56.02%), Space: 44.8 MB (83.11%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.js
+++ 200-number-of-islands/200-number-of-islands.js
@@ -4,20 +4,20 @@
  */
 var numIslands = function(grid) {
     
-    let numOfIslands = 0;
+    let numberOfIslands = 0;
     
     for(let row = 0; row < grid.length; row++){
         for(let col = 0; col < grid[0].length; col++){
             if(grid[row][col] === "1"){
+                numberOfIslands++;
                 dfs(row, col);
-                numOfIslands++;
             }
         }
     }
     
     function dfs(row, col){
         if(grid?.[row]?.[col] === "1"){
-            grid[row][col] = "2";
+            grid[row][col] = 2
             
             dfs(row + 1, col);
             dfs(row - 1, col);
@@ -26,6 +26,6 @@ var numIslands = function(grid) {
         }
     }
     
-    return numOfIslands;
+    return numberOfIslands;
     
 };
