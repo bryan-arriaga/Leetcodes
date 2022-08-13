347f30bafc2a3e5b85c36fa89a43dc916ad9fa2a
Bryan Arriaga <50850782+aHumptyDumpty@users.noreply.github.com>
2022-08-13 15:07:53 -0500

    Time: 88 ms (49.71%), Space: 41.8 MB (89.53%) - LeetHub

--- 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.js
+++ 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.js
@@ -2,19 +2,21 @@
  * @param {number[]} nums
  * @return {number}
  */
-var findMin = function(nums) {
+var findMin = function(nums) { 
+    if(nums=== null) return -1
     
     let left = 0;
     let right = nums.length - 1;
     
     while(left < right){
-        let middle = Math.floor((left + right)/2 );
-        
-        if(nums[middle] > nums[right]) left = middle + 1
-        else right = middle
+        let middle = Math.floor((left + right) / 2);
+        if(nums[middle] > nums[right]){
+            left = middle + 1;
+        } else {
+            right = middle
+        }
     }
     
     return nums[left]
-
     
 };
