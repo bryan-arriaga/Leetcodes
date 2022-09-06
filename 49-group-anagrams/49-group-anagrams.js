df2c4ba9bd36a51ce2d3a4ccf5d6d979d15ca00a
Bryan Arriaga <50850782+aHumptyDumpty@users.noreply.github.com>
2022-09-06 12:43:32 -0500

    Time: 238 ms (28.44%), Space: 54.2 MB (36.70%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.js
+++ 49-group-anagrams/49-group-anagrams.js
@@ -4,31 +4,19 @@
  */
 var groupAnagrams = function(strs) {
     
-    if(strs.length <= 0 || strs === null) return 0;
+    //create a object to hold every string 
+    let map = {};
     
-    //create a map 
-    //create answer arr to return at the end
-    let map = new Map();
-    let arr = [];
+    //for loop iterate through strs
+    for(let str of strs){
+    //create a sorted var 
+    let sorted = str.split("").sort().join("");
     
-    //iterate through strs and make new const for sorted strings
-    for(let i = 0; i < strs.length; i++){
-        const sorted = strs[i].split("").sort().join("")
-        
-        if(map.has(sorted)){
-            map.get(sorted).push(strs[i])
-        } else {
-            map.set(sorted, [strs[i]])
-        }
+    map[sorted] ? map[sorted].push(str) : map[sorted] = [str];
+    
     }
-    
 
     
-    //iterate and append the values from map to answer array
-    for(let [key, value] of map){
-        arr.push(value);
-    }
-    
-    return arr;
-    
+    //return object.values of map
+    return Object.values(map)
 };
