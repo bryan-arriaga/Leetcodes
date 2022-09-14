cc74794b37351b81e4ae54de599d439334518583
Bryan Arriaga <50850782+aHumptyDumpty@users.noreply.github.com>
2022-09-13 19:09:47 -0500

    Time: 133 ms (50.49%), Space: 45.8 MB (47.83%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.js
+++ 200-number-of-islands/200-number-of-islands.js
@@ -3,26 +3,22 @@
  * @return {number}
  */
 var numIslands = function(grid) {
-    
+    //create a var for the output
     let numberOfIslands = 0;
     
-    //loop through starting at top left
+    //iterate through grid for rows and cols
     for(let row = 0; row < grid.length; row++){
         for(let col = 0; col < grid[0].length; col++){
-            if(grid[row][col] === "1"){
+            if(grid?.[row]?.[col] === "1"){
                 numberOfIslands++;
                 dfs(row, col)
             }
         }
     }
     
-    
-    //dfs 1 for matching vertical horizontal 1's
     function dfs(row, col){
-        //
         if(grid?.[row]?.[col] === "1"){
-            //set the value = to something else besides 1 or null
-            grid[row][col] = "2"
+            grid[row][col] = "2";
             
             dfs(row + 1, col);
             dfs(row - 1, col);
@@ -31,5 +27,7 @@ var numIslands = function(grid) {
         }
     }
     
-    return numberOfIslands;
+    
+    return numberOfIslands 
+    
 };
